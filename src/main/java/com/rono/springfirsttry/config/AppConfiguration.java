7da935faf749fa02526b2c1d2d7cb11c2b2fbd06
AlexRono <klushalex@gmail.com>
2020-08-16 12:37:41 +0300

    first working version

--- src/main/java/com/rono/springfirsttry/config/AppConfiguration.java
+++ src/main/java/com/rono/springfirsttry/config/AppConfiguration.java
@@ -1,6 +1,7 @@
 package com.rono.springfirsttry.config;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AvailableSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -74,19 +77,19 @@ public class AppConfiguration {
             return intPropertyValue;
         }
 
-        @Bean
-        public LocalSessionFactoryBean getSessionFactory() {
-            LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-            sessionFactory.setDataSource(getDataSource());
-            sessionFactory.setPackagesToScan(new String[] { "com.bytestree.model" });
-            sessionFactory.setHibernateProperties(getHibernateProperties());
-            return sessionFactory;
-         }
-
-        @Bean
-        public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
-            HibernateTransactionManager txManager = new HibernateTransactionManager();
-            txManager.setSessionFactory(sessionFactory);
-            return txManager;
-        }
+//        @Bean
+//        public LocalSessionFactoryBean getSessionFactory() {
+//            LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+//            sessionFactory.setDataSource(getDataSource());
+//            sessionFactory.setPackagesToScan(new String[] { "com.bytestree.model" });
+//            sessionFactory.setHibernateProperties(getHibernateProperties());
+//            return sessionFactory;
+//         }
+//
+//        @Bean
+//        public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+//            HibernateTransactionManager txManager = new HibernateTransactionManager();
+//            txManager.setSessionFactory(sessionFactory);
+//            return txManager;
+//        }
 }
